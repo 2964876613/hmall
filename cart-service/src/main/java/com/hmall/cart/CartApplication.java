7468cba415b27aa1474a070c0f87a13b4d22785f
Perth <zxu197310@gmail.com>
2026-09-16 18:42:42 +0800

    OpenFeign

--- cart-service/src/main/java/com/hmall/cart/CartApplication.java
+++ cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.hmall.api.client"/*, defaultConfiguration = DefaultFeignConfig.class*/)
 @MapperScan("com.hmall.cart.mapper")
 @SpringBootApplication
 public class CartApplication {
--- cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
+++ cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
@@ -1,11 +1,11 @@
 package com.hmall.cart.service.impl;
 
-import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmall.api.client.ItemClient;
+import com.hmall.api.dto.ItemDTO;
 import com.hmall.cart.domain.dto.CartFormDTO;
-import com.hmall.cart.domain.dto.ItemDTO;
 import com.hmall.cart.domain.po.Cart;
 import com.hmall.cart.domain.vo.CartVO;
 import com.hmall.cart.mapper.CartMapper;
@@ -15,15 +15,8 @@ import com.hmall.common.utils.BeanUtils;
 import com.hmall.common.utils.CollUtils;
 import com.hmall.common.utils.UserContext;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -43,8 +36,9 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements ICartService {
 
-    private final RestTemplate restTemplate;
-    private final DiscoveryClient discoveryClient;
+/*    private final RestTemplate restTemplate;
+    private final DiscoveryClient discoveryClient;*/
+    private final ItemClient itemClient;
 
     @Override
     public void addItem2Cart(CartFormDTO cartFormDTO) {
@@ -88,10 +82,9 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
     }
 
     private void handleCartItems(List<CartVO> vos) {
-        // TODO 1.获取商品id
         // 1.获取商品id
         Set<Long> itemIds = vos.stream().map(CartVO::getItemId).collect(Collectors.toSet());
-        // 2.查询商品
+/*        // 2.查询商品
         // 2.1.根据服务名称获取服务的实例列表
         List<ServiceInstance> instances = discoveryClient.getInstances("item-service");
         if (instances == null || instances.isEmpty()) {
@@ -101,7 +94,7 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
         ServiceInstance instance = instances.get(RandomUtil.randomInt(instances.size()));
         URI uri = instance.getUri();
 
-        // 2.1.利用RestTemplate发起http请求，得到http的响应
+        // 2.3.利用RestTemplate发起http请求，得到http的响应
         ResponseEntity<List<ItemDTO>> response = restTemplate.exchange(
                 uri + "/items?ids={ids}",
                 HttpMethod.GET,
@@ -110,12 +103,13 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
                 },
                 Map.of("ids", CollUtils.join(itemIds, ","))
         );
-        // 2.2.解析响应
+        // 2.4.解析响应
         if(!response.getStatusCode().is2xxSuccessful()){
             // 查询失败，直接结束
             return;
         }
-        List<ItemDTO> items = response.getBody();
+        List<ItemDTO> items = response.getBody();*/
+        List<ItemDTO> items = itemClient.queryItemByIds(itemIds);
         if (CollUtils.isEmpty(items)) {
             return;
         }
